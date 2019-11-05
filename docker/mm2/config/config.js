d79dadc6a8b5b8b798887168eaed67b9de060a41
Wayward One <waywardone@gmail.com>
2019-11-04 23:45:01 -0800

    Update config

--- docker/mm2/config/config.js
+++ docker/mm2/config/config.js
@@ -9,13 +9,13 @@
  */
 
 var config = {
-    address: "localhost", // Address to listen on, can be:
+    address: "", // Address to listen on, can be:
     // - "localhost", "127.0.0.1", "::1" to listen on loopback interface
     // - another specific IPv4/6 to listen on a specific interface
     // - "", "0.0.0.0", "::" to listen on any interface
     // Default, when address config is left out, is "localhost"
     port: 8080,
-    ipWhitelist: ["127.0.0.1", "::ffff:127.0.0.1", "::1"], // Set [] to allow all IP addresses
+    ipWhitelist: ["127.0.0.1", "::ffff:127.0.0.1", "::1", "::ffff:192.168.1.0/24"], // Set [] to allow all IP addresses
     // or add a specific IPv4 of 192.168.1.5 :
     // ["127.0.0.1", "::ffff:127.0.0.1", "::1", "::ffff:192.168.1.5"],
     // or IPv4 range of 192.168.3.0 --> 192.168.3.15 use CIDR format :
@@ -23,7 +23,7 @@ var config = {
 
     language: "en",
     timeFormat: 24,
-    units: "metric",
+    units: "imperial",
 
     modules: [
         {
@@ -35,7 +35,10 @@ var config = {
         },
         {
             module: "clock",
-            position: "top_left"
+            position: "top_left",
+            config: {
+                timezone: "America/Los_Angeles"
+            }
         },
         {
             module: "calendar",
@@ -45,21 +48,20 @@ var config = {
                 calendars: [
                     {
                         symbol: "calendar-check",
-                        url: "webcal://www.calendarlabs.com/ical-calendar/ics/76/US_Holidays.ics"					}
+                        url: "webcal://www.calendarlabs.com/ical-calendar/ics/76/US_Holidays.ics",
+                        maximumNumberOfDays: 30,
+                        maximumEntries: 2
+                    }
                 ]
             }
-        },
-        {
-            module: "compliments",
-            position: "lower_third"
         },
         {
             module: "currentweather",
             position: "top_right",
             config: {
-                location: "New York",
-                locationID: "",  //ID from http://bulk.openweathermap.org/sample/city.list.json.gz; unzip the gz file and find your city
-                appid: "YOUR_OPENWEATHER_API_KEY"
+                location: "San Jose",
+                locationID: "5392171",  //ID from http://bulk.openweathermap.org/sample/city.list.json.gz; unzip the gz file and find your city
+                appid: "Update me"
             }
         },
         {
@@ -67,9 +69,10 @@ var config = {
             position: "top_right",
             header: "Weather Forecast",
             config: {
-                location: "New York",
-                locationID: "5128581",  //ID from http://bulk.openweathermap.org/sample/city.list.json.gz; unzip the gz file and find your city
-                appid: "YOUR_OPENWEATHER_API_KEY"
+                location: "San Jose",
+                locationID: "5392171",  //ID from http://bulk.openweathermap.org/sample/city.list.json.gz; unzip the gz file and find your city
+                appid: "Update me",
+                maxNumberOfDays: 1
             }
         },
         {
@@ -80,6 +83,10 @@ var config = {
                     {
                         title: "New York Times",
                         url: "http://www.nytimes.com/services/xml/rss/nyt/HomePage.xml"
+                    },
+                    {
+                        title: "ESPN Tennis",
+                        url: "https://www.espn.com/espn/rss/tennis/news"
                     }
                 ],
                 showSourceTitle: true,
@@ -88,9 +95,39 @@ var config = {
                 broadcastNewsUpdates: true
             }
         },
+        {
+            module: "MMM-JSONStatusChecker",
+            position: "bottom_left",
+            header: "Garage Door",
+            config: {
+                name: "opengarage_status",
+                updateInterval: 60 * 1000, // Check every minute
+                urlApi: "http://update.me/jc",
+                keyToCheck: "door",
+                keyValue: 0,
+                trueString: "Closed",
+                falseString: "Open",
+                icon: "warehouse"
+            }
+        },
+        {
+            module: "MMM-Dad-Jokes",
+            position: "bottom_bar",
+            config: {
+                updateInterval: 60000
+            }
+        },
+        {
+            module: "MMM-Wallpaper",
+            position: "fullscreen_below",
+            config: {
+                source: "bing",
+                slideInterval: 60 * 10 * 1000 // Change every 10 minutes
+            }
+        },
     ]
 
 };
 
 /*************** DO NOT EDIT THE LINE BELOW ***************/
-if (typeof module !== "undefined") {module.exports = config;}
+if (typeof module !== "undefined") { module.exports = config; }
